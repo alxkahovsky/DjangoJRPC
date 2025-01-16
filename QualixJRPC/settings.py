42d7b9a58aa8c7e30b07a09dc43f46e7de5dc2be
Alex Kahovsky <alxkahovsky@gmail.com>
2025-01-16 21:14:59 +0300

    [DEVELOP] настройка статики, включил админку

--- QualixJRPC/settings.py
+++ QualixJRPC/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'jrpc_client.apps.JrpcClientConfig'
 ]
 
@@ -116,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_dev'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_prod')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
